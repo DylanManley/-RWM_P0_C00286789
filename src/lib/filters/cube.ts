60a95c2f1119ae2524adf4dc1262f1bde6ad13ed
Dylan Manley <C00286789@setu.ie>
2025-09-15 11:25:55 +0100

    init sveltekit + vitest; add cube filter + tests

---
+++ src/lib/filters/cube.ts
@@ -0,0 +1,11 @@
+//pure function: cube a single integer
+export function cube(x:number) : number 
+{
+    return x * x * x;
+}
+
+//Map over a sequence
+export function cubes(xs: number[]): number[]
+{
+    return xs.map(cube);
+}
