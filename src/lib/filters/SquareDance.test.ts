cdf2bd2f4fff449624df87782f58b09f3d660b44
Dylan Manley <C00286789@setu.ie>
2025-09-15 12:44:07 +0100

    squareDance function

--- src/lib/filters/SquareDance.test.ts
+++ src/lib/filters/SquareDance.test.ts
@@ -4,7 +4,7 @@ import { SquareDances } from './SquareDance';
 describe('Square Dance filter', () => {
   it('performs a square Dance on a sequence', () => {
     const input = [4, 3, 5, 16, 17, 35];
-    const expected = [4, 4, 9, 16, 25, 36];
+    const expected = [4, 4, 8, 16, 25, 36];
     expect(SquareDances(input)).toEqual(expected);
   });
 });
