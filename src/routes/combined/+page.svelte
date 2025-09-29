201de0b1cff5a36ed4a1822341d8927a1a9d768c
Dylan Manley <C00286789@setu.ie>
2025-09-29 11:22:05 +0100

    fixed sprint 2

--- src/routes/combined/+page.svelte
+++ src/routes/combined/+page.svelte
@@ -1,29 +1,13 @@
 <p>Visit <a href="/" style = "font-weight: bold; text-decoration: underline;">Solo</a></p>
 
-
-
-
-
 <script lang="ts">
 
-  import { cubes } from '$lib/filters';
-let raw = '1 3 5 7 9 999';
-$: input = raw.trim().split(/\s+/).filter(Boolean).map(Number);
-$: output = cubes(input);
-
-
-  
   import { combinedFilter } from '$lib/filters/combined/combined';
-  let Combinedraw = '1 3 5 7 9';
-  $: Combinedinput = raw.trim().split(/\s+/).filter(Boolean).map(Number);
-  $: Combinedoutput = combinedFilter(input);
+  let raw = '1 3 5 7 9';
+  $: input = raw.trim().split(/\s+/).filter(Boolean).map(Number);
+  $: output = combinedFilter(input);
 </script>
 
-
-
-<h1>Mine</h1>
-<label>Input <input bind:value={raw} /></label>
-<p>Output: {output.join(' ')}</p>
 
 <h1>Combined (mine → peer)</h1>
 <label>Input <input bind:value={raw} /></label>
